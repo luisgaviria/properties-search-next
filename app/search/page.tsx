37669ac6e2ea315f1af8da6117e90aa2aeb879cb
Patryk404 <patryk744@op.pl>
2024-03-15 22:52:21 +0100

    fix: removed suspense for search filters

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,11 +5,11 @@ import Loading from "../loading";
 export default function Search() {
   return (
     <>
-      <Suspense fallback={<Loading />}>
+      {/* <Suspense fallback={<Loading />}> */}
         <Filters cityData={null} cityPages={0}/> {
           // doesnt matter what is params here it won't use it in that case
         }
-      </Suspense>
+      {/* </Suspense> */}
     </>
   );
 }
