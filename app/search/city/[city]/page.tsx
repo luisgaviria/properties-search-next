8342562c42b5a720483cf767381a28643baecc15
Patryk404 <patryk744@op.pl>
2024-03-18 21:58:51 +0100

    feat: test

--- app/search/city/[city]/page.tsx
+++ app/search/city/[city]/page.tsx
@@ -4,15 +4,15 @@ import { Metadata } from "next";
 import { headers } from 'next/headers';
 import { usePathname } from "next/navigation";
 
-export async function generateMetadata({params,searchParams}:{params: any;searchParams: any}): Promise<Metadata> {
-    const city = params.city;
+// export async function generateMetadata({params,searchParams}:{params: any;searchParams: any}): Promise<Metadata> {
+//     const city = params.city;
      
-    const data = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/api/search/mlspin?City=${city}&page=1&PropertyType=Residential,Residential Income`,{cache: "no-store"}).then(res=>res.json());
-    const totalAmountOfProperties = data.pages*12;
-    return { 
-        title: `There are ${totalAmountOfProperties} for sale in ${city}`
-    }
-}
+//     const data = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/api/search/mlspin?City=${city}&page=1&PropertyType=Residential,Residential Income`,{cache: "no-store"}).then(res=>res.json());
+//     const totalAmountOfProperties = data.pages*12;
+//     return { 
+//         title: `There are ${totalAmountOfProperties} for sale in ${city}`
+//     }
+// }
 
 async function getCityListings(city: any){
     let query = "";
