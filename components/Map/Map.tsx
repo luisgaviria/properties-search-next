c2496ff8d6ced15a8c821ef51cae134fbdd373d2
luisgaviria <luis.aptx@gmail.com>
2024-08-10 09:27:09 -0400

    info window changes

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -150,35 +150,35 @@ class GoogleMapContainer extends Component<GoogleMapContainerProps> {
             <a href="/search/${
               property.ListingId
             }" target="_blank" style="text-decoration:none;">
-      <div style="padding: 15px; max-width: 300px; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); font-family: Arial, sans-serif;">
-        <div style="padding: 10px; border-radius: 5px;" class="marker">
-          <img style="width: 100%; height: 200px; object-fit: cover;" src="${imgUrl}" alt="Property"/>
-          <br/>
-          <div style="display: flex; justify-content: space-between; align-items: flex-start;">
-            <div>
-              <div style="font-size: 16px; font-weight: 500; margin-bottom: 5px;">
+            <div style="padding: 15px; max-width: 300px; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); font-family: Arial, sans-serif;">
+              <div style="padding: 10px; border-radius: 5px;" class="marker">
+               <img style="width: 100%; height: 200px; object-fit: cover; border-radius: 5px;" src="${imgUrl}" alt="Property"/>
+               <br/>
+               <div style="display: flex; justify-content: space-between; align-items: flex-start;">
+               <div>
+                <div style="font-size: 16px; font-weight: 500; margin-bottom: 5px;">
                 ${property.StreetNumber} ${property.StreetName}
-              </div>
-              <div style="color: #555; font-size: 14px;">
+                </div>
+                <div style="color: #555; font-size: 14px;">
                 ${property.City}
-              </div>
-            </div>
-            <div style="text-align: right;">
-              <div style="font-size: 16px; font-weight: bold;">
-                $${property.ListPrice.toLocaleString()}
-              </div>
+                 </div>
+               </div>
+              <div style="text-align: right;">
+               <div style="font-size: 16px; font-weight: bold;">
+                 $${property.ListPrice.toLocaleString()}
+               </div>
               <div style="color: #555; font-size: 14px;">
                 ${
                   property.LivingArea
                     ? `${property.LivingArea.toLocaleString()} sqft`
                     : ""
                 }
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </a>
+      </a>
           `,
           }),
         };
