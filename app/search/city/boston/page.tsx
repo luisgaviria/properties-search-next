7dff48b534dbb4e33fb1847b6ace12f26aad280c
Patryk404 <patryk744@op.pl>
2024-08-12 22:47:23 +0200

    feat: working in progress with search city

--- app/search/city/boston/page.tsx
+++ app/search/city/boston/page.tsx
@@ -1,45 +1,50 @@
-import Filters from "@/components/Mlspin/Filters/Filters";
-// import { response } from "@/components/definitions/Filters";
-import { Metadata } from "next";
-import { headers } from "next/headers";
-import Head from "next/head";
+// import Filters from "@/components/Mlspin/Filters/Filters";
+// // import { response } from "@/components/definitions/Filters";
 
-// export async function generateMetadata({params,searchParams}:{params: any;searchParams: any}): Promise<Metadata> {
-//     const city = params.city;
+// import Head from "next/head";
 
-//     const data = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/api/search/mlspin?City=${city}&page=1&PropertyType=Residential,Residential Income`,{cache: "no-store"}).then(res=>res.json());
-//     const totalAmountOfProperties = data.pages*12;
-//     return {
-//         title: `There are ${totalAmountOfProperties} for sale in ${city}`
-//     }
+// // export async function generateMetadata({params,searchParams}:{params: any;searchParams: any}): Promise<Metadata> {
+// //     const city = params.city;
+
+// //     const data = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/api/search/mlspin?City=${city}&page=1&PropertyType=Residential,Residential Income`,{cache: "no-store"}).then(res=>res.json());
+// //     const totalAmountOfProperties = data.pages*12;
+// //     return {
+// //         title: `There are ${totalAmountOfProperties} for sale in ${city}`
+// //     }
+// // }
+
+// async function getCityListing() {
+//   let query = "";
+//   query += `City=boston`; // ${city} -> previously!
+//   query += `&PropertyType=Residential,Residential Income`;
+//   query += "&save=true";
+//   const res: any = await fetch(
+//     `${process.env.NEXT_PUBLIC_URL_API}:${process.env.PORT || 3000}/api/search/mlspin?${query}&page=1`,
+//     {
+//       cache: "no-store",
+//     },
+//   ).then((res) => res.json());
+
+//   return { properties: res.properties, pages: res.pages };
 // }
 
-async function getCityListing() {
-  let query = "";
-  query += `City=boston`; // ${city} -> previously!
-  query += `&PropertyType=Residential,Residential Income`;
-  query += "&save=true";
-  const res: any = await fetch(
-    `${process.env.NEXT_PUBLIC_URL_API}:${process.env.PORT || 3000}/api/search/mlspin?${query}&page=1`,
-    {
-      cache: "no-store",
-    },
-  ).then((res) => res.json());
+// export const maxDuration = 30;
 
-  return { properties: res.properties, pages: res.pages };
-}
+// export default async function Boston() {
+//   // on vercel it is not sending as a header x-invoke-path we need to create middleware to add one more header with request url
+//   const data = await getCityListing();
+//   return (
+//     <>
+//       <Head>
+//         <title>{`There are ${data.properties.length} for sale in Boston`}</title>
+//       </Head>
+//       <Filters cityData={data.properties} cityPages={data.pages} />
+//     </>
+//   );
+// }
 
-export const maxDuration = 30;
-
-export default async function ExactCity() {
-  // on vercel it is not sending as a header x-invoke-path we need to create middleware to add one more header with request url
-  const data = await getCityListing();
+export default function Hello(){
   return (
-    <>
-      <Head>
-        <title>{`There are 12  for sale in Boston`}</title>
-      </Head>
-      <Filters cityData={data.properties} cityPages={data.pages} />
-    </>
-  );
-}
+    <h2>Currently in progress...</h2>
+  )
+};
