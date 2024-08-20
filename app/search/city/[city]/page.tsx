df02a2203011d5e928bb30e70e25dc43bed7970f
luisgaviria <luis.aptx@gmail.com>
2024-08-20 18:11:00 -0400

    update metadata on cities

--- app/search/city/[city]/page.tsx
+++ app/search/city/[city]/page.tsx
@@ -1,53 +1,91 @@
-import fs from "fs"
-import Head from "next/head";
+import fs from "fs";
 import Filters from "@/components/Mlspin/Filters/Filters";
 
-
 async function getCityListing(city: string) {
-    let query = "";
-    query += `City=${city}`; // ${city} -> previously!
-    query += `&PropertyType=Residential,Residential Income`;
-    query += "&save=true";
-    const res: any = await fetch(
-      `https://www.bostonharmonyhomes.com/api/search/mlspin?${query}&page=1`,
-      {
-        cache: "no-store",
-      },
-    ).then((res) => res.json());
-  
-    return { properties: res.properties, pages: res.pages };
-  }
-
-export async function generateMetadata({ params }: {params: {city: string}}) {
-    const city = params.city; 
-    const data = await fetch(`https://www.bostonharmonyhomes.com/api/search/mlspin?City=${city}&page=1&PropertyType=Residential,Residential Income`,{cache: "no-store"}).then(res=>res.json());
-    // we should use an exisitng api not the one which is in development because the one in development is not running and it is not working!
-    const totalAmountOfProperties = data.pages*12;
-  
-    return {
-        title: `There are ${totalAmountOfProperties} for sale in ${city}`
+  let query = "";
+  query += `City=${city}`; // ${city} -> previously!
+  query += `&PropertyType=Residential,Residential Income`;
+  query += "&save=true";
+  const res: any = await fetch(
+    `https://www.bostonharmonyhomes.com/api/search/mlspin?${query}&page=1`,
+    {
+      cache: "no-store",
     }
+  ).then((res) => res.json());
+
+  return { properties: res.properties, pages: res.pages };
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { city: string };
+}) {
+  const city = params.city;
+  const data = await fetch(
+    `https://www.bostonharmonyhomes.com/api/search/mlspin?City=${city}&page=1&PropertyType=Residential,Residential Income`,
+    { cache: "no-store" }
+  ).then((res) => res.json());
+  const totalAmountOfProperties = data.pages * 12;
+
+  const title = `Boston Harmony Homes: ${totalAmountOfProperties} Properties for Sale in ${
+    city.charAt(0).toUpperCase() + city.slice(1)
+  }`;
+  const description = `Explore ${totalAmountOfProperties} real estate listings in ${
+    city.charAt(0).toUpperCase() + city.slice(1)
+  } with Boston Harmony Homes. Buy or sell with market insights, detailed photos, and advanced filters.`;
+  const url = `https://www.bostonharmonyhomes.com/search/city/${city}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      type: "website",
+      url,
+      title,
+      description,
+      images: [
+        {
+          // url: 'https://www.bostonharmonyhomes.com/images/default-image.jpg', // Placeholder URL for now
+          alt: `Real estate listings in ${
+            city.charAt(0).toUpperCase() + city.slice(1)
+          }`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@BostonHarmonyHomes", // Update with your Twitter handle
+      title,
+      description,
+      // image: "https://www.bostonharmonyhomes.com/images/default-image.jpg", // Placeholder URL for now
+    },
+  };
 }
 
 export async function generateStaticParams() {
-    const file = fs.readFileSync(process.cwd() + '/data/massachusetsCities.json','utf8');
-    const data = JSON.parse(file);
+  const file = fs.readFileSync(
+    process.cwd() + "/data/massachusetsCities.json",
+    "utf8"
+  );
+  const data = JSON.parse(file);
 
-    return data.map((city: any) => ({
-        city: city.Name.toLowerCase(),
-    }));
+  return data.map((city: any) => ({
+    city: city.Name.toLowerCase(),
+  }));
 }
 
-export default async function CityPage({ params }: {params: {city: string}}) {
-    const city = params.city;
-    const data = await getCityListing(city);
-  
-    return (
-        <>
-        <Head>
-          <title>{`There are 12  for sale in Boston`}</title>
-        </Head>
-        <Filters cityData={data.properties} cityPages={data.pages} />
-      </>
-    );
-  }
+export default async function CityPage({
+  params,
+}: {
+  params: { city: string };
+}) {
+  const city = params.city;
+  const data = await getCityListing(city);
+
+  return (
+    <>
+      <Filters cityData={data.properties} cityPages={data.pages} />
+    </>
+  );
+}
